eff081c25ae2fdacb6d2acb869c7575055104dda
ahmed <ahmed.zaakan@gmail.com>
2024-10-25 00:24:48 +0300

    add ainmation

--- src/compenent/Home/Home.js
+++ src/compenent/Home/Home.js
@@ -5,8 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { faLocationArrow } from '@fortawesome/free-solid-svg-icons';
-import Fade from 'react-reveal/Fade';
-
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -29,6 +27,7 @@ const Home = () => {
       })
       .catch(error => {
         console.log('Error fetching data:', error);
+        // يمكنك هنا إضافة منطق لإظهار رسالة للمستخدم
       });
   }, []);
 
@@ -48,28 +47,27 @@ const Home = () => {
             <h2>جميع القوالب</h2>
           </div>
           <div className="row rowhome">
-          <Fade top>
             {filteredItems
               .sort((a, b) => new Date(b.date) - new Date(a.date)) // ترتيب العناصر بترتيب تنازلي
-              .map((item, index) => ( // استخدام .map() مباشرة بعد الترتيب
-                <div className="col-lg-4 col-md-6 col-sm-12" key={index}>
-                  <div className="card cardlastprojict">
-                    <a href={item.view} target="_blank" rel="noopener noreferrer">
-                      <img className="card-img-top" src={item.img} alt={item.title} />
-                      <div className="card-body latestworks-body">
-                        <h5 className="card-title latestworks-h5">{item.title}</h5>
-                        <p className="card-text latestworks-p">{item.description}</p>
-                      </div>
-                    </a>
-                    <ul>
-                      <li><a href={item.download} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="coloricon"/></a></li>
-                      <li><a href={item.view} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEye} className="coloricon" /></a></li>
-                      <li><a href={item.qalip} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLocationArrow} className="coloricon"/></a></li>
-                    </ul>
-                  </div>
+              .map((item, index) => (
+                 // استخدام .map() مباشرة بعد الترتيب
+                <div className="col-lg-4 col-md-6 col-sm-12" key={item.id || index}>
+                    <div className="card cardlastprojict">
+                      <a href={item.view} target="_blank" rel="noopener noreferrer">
+                        <img className="card-img-top" src={item.img} alt={item.title} />
+                        <div className="card-body latestworks-body">
+                          <h5 className="card-title latestworks-h5">{item.title}</h5>
+                          <p className="card-text latestworks-p">{item.description}</p>
+                        </div>
+                      </a>
+                      <ul>
+                        <li><a href={item.download} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="coloricon"/></a></li>
+                        <li><a href={item.view} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEye} className="coloricon" /></a></li>
+                        <li><a href={item.qalip} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLocationArrow} className="coloricon"/></a></li>
+                      </ul>
+                    </div>
                 </div>
               ))}
-                        </Fade>
           </div>
         </div>
       </div>
